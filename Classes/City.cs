a3780a0a5d9b111f9feea5e9b5ae4edca58e6196
JoachimKvamme <kaj082024@gmail.com>
2024-09-25 09:53:40 +0200

    initial commit, og oppgave 1

---
+++ Classes/City.cs
@@ -0,0 +1,9 @@
+
+
+public class City {
+    public string? Name {get; set;}
+    public string? Country {get; set;}
+
+    public int population {get; set;}
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace filbehandling_oppgaver;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        WriteToFile("Heihei", "oppgavefil.txt");
+
+
+
+    }
+
+    // Oppgave 1
+
+    // Denne metoden tar to parametre: En streng som skal skrives til en fil, og en streng som representerer
+    // filstien til filen den skriver til.
+    // (Jeg så øyeblikkelig etter å ha laget metoden at File.WriteAllText gjør nøyaktig det samme, bare med
+    // omvendt rekkefølge på parametrene.)
+
+    static void WriteToFile(string melding, string filsti) {
+        File.WriteAllText(filsti, melding);
+    }
+
+    // Oppgave 2
+    
+}
